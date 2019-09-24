4729e22c2e4991b1408d8faaac82fe6e3e4945de
LnB11855 <33381018+LnB11855@users.noreply.github.com>
2019-09-23 22:27:07 -0500

    Update data_process.py

--- data_process.py
+++ data_process.py
@@ -9,7 +9,6 @@ for i in range(14):
         data=pd.read_csv(filename)
         df = pd.concat([df, data], ignore_index=True, axis=0)
 print(np.max(df["val_acc"]))
-# df["val_acc"][390:]=df["val_acc"][390:]+0.01
 plt.plot(df["acc"])
 plt.plot(df["val_acc"])
 plt.title('Prediction Accuracy')
